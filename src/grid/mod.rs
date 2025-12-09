6c4030ae02f38244b1fc0cfffa980d58caab911e
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-12-09 08:46:12 -0500

    Add Point to grid

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,4 +1,23 @@
 use std::fmt::Display;
+use std::io::Write;
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+impl From<Point> for (usize, usize) {
+    fn from(value: Point) -> Self {
+        (value.x, value.y)
+    }
+}
 
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
 pub enum Direction {
@@ -30,76 +49,78 @@ impl From<Direction> for char {
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Neighbor {
     pub direction: Direction,
-    pub position: (usize, usize),
+    pub position: Point,
 }
 
 impl Neighbor {
-    pub fn new(direction: Direction, x: usize, y: usize) -> Self {
+    pub fn new(direction: Direction, position: Point) -> Self {
         Self {
             direction,
-            position: (x, y),
+            position,
         }
     }
 
     pub fn next<T>(self, grid: &[Vec<T>]) -> Option<Neighbor> {
         let Neighbor {
             direction,
-            position: (x, y),
+            position,
         } = self;
+        let x = position.x;
+        let y = position.y;
 
         match direction {
             Direction::Right => {
                 if grid.get(y).and_then(|r| r.get(x + 1)).is_some() {
-                    Some(Self::new(Direction::Right, x + 1, y))
+                    Some(Self::new(Direction::Right, Point::new(x + 1, y)))
                 } else {
                     None
                 }
             }
             Direction::Left => {
                 if grid.get(y).is_some() && x > 0 {
-                    Some(Self::new(Direction::Left, x - 1, y))
+                    Some(Self::new(Direction::Left, Point::new(x - 1, y)))
                 } else {
                     None
                 }
             }
             Direction::Up => {
                 if y > 0 {
-                    Some(Self::new(Direction::Up, x, y - 1))
+                    Some(Self::new(Direction::Up, Point::new(x, y - 1)))
                 } else {
                     None
                 }
             }
             Direction::Down => {
                 if grid.get(y + 1).and_then(|r| r.get(x)).is_some() {
-                    Some(Self::new(Direction::Down, x, y + 1))
+                    Some(Self::new(Direction::Down, Point::new(x, y + 1)))
                 } else {
                     None
                 }
             }
             Direction::UpperRight => {
                 if y > 0 && grid[y - 1].get(x + 1).is_some() {
-                    Some(Self::new(Direction::UpperRight, x + 1, y - 1))
+                    Some(Self::new(Direction::UpperRight, Point::new(x + 1, y - 1)))
                 } else {
                     None
                 }
             }
             Direction::UpperLeft => {
                 if y > 0 && x > 0 {
-                    Some(Self::new(Direction::UpperLeft, x - 1, y - 1))
+                    Some(Self::new(Direction::UpperLeft, Point::new(x - 1, y - 1)))
                 } else {
                     None
                 }
             }
             Direction::LowerRight => {
                 if grid.get(y + 1).and_then(|r| r.get(x + 1)).is_some() {
-                    Some(Self::new(Direction::LowerRight, x + 1, y + 1))
+                    Some(Self::new(Direction::LowerRight, Point::new(x + 1, y + 1)))
                 } else {
                     None
                 }
             }
             Direction::LowerLeft => {
                 if grid.get(y + 1).is_some() && x > 0 {
-                    Some(Self::new(Direction::LowerLeft, x - 1, y + 1))
+                    Some(Self::new(Direction::LowerLeft, Point::new(x - 1, y + 1)))
                 } else {
                     None
                 }
@@ -108,45 +129,59 @@ impl Neighbor {
     }
 }
 
-impl From<Neighbor> for (usize, usize) {
+impl From<Neighbor> for Point {
     fn from(value: Neighbor) -> Self {
         value.position
+    }
+}
+
+impl From<Neighbor> for (usize, usize) {
+    fn from(value: Neighbor) -> Self {
+        value.position.into()
     }
 }
 
 pub fn neighbor_in_direction<T>(
     grid: &[Vec<T>],
     direction: Direction,
-    x: usize,
-    y: usize,
+    position: Point,
 ) -> Option<Neighbor> {
+    let x = position.x;
+    let y = position.y;
+
     match direction {
-        Direction::Up => y.checked_sub(1).map(|y| Neighbor::new(direction, x, y)),
-        Direction::Down => grid.get(y + 1).map(|_| Neighbor::new(direction, x, y + 1)),
-        Direction::Left => x.checked_sub(1).map(|x| Neighbor::new(direction, x, y)),
+        Direction::Up => y
+            .checked_sub(1)
+            .map(|y| Neighbor::new(direction, Point::new(x, y))),
+        Direction::Down => grid
+            .get(y + 1)
+            .map(|_| Neighbor::new(direction, Point::new(x, y + 1))),
+        Direction::Left => x
+            .checked_sub(1)
+            .map(|x| Neighbor::new(direction, Point::new(x, y))),
         Direction::Right => grid[y]
             .get(x + 1)
-            .map(|_| Neighbor::new(direction, x + 1, y)),
+            .map(|_| Neighbor::new(direction, Point::new(x + 1, y))),
         Direction::UpperLeft => y
             .checked_sub(1)
             .filter(|_| x > 0)
-            .map(|y| Neighbor::new(direction, x - 1, y)),
+            .map(|y| Neighbor::new(direction, Point::new(x - 1, y))),
         Direction::UpperRight => y
             .checked_sub(1)
             .and_then(|y| grid[y].get(x + 1))
-            .map(|_| Neighbor::new(direction, x + 1, y - 1)),
+            .map(|_| Neighbor::new(direction, Point::new(x + 1, y - 1))),
         Direction::LowerLeft => grid
             .get(y + 1)
             .filter(|_| x > 0)
-            .map(|_| Neighbor::new(direction, x - 1, y + 1)),
+            .map(|_| Neighbor::new(direction, Point::new(x - 1, y + 1))),
         Direction::LowerRight => grid
             .get(y + 1)
             .and_then(|_| grid[y + 1].get(x + 1))
-            .map(|_| Neighbor::new(direction, x + 1, y + 1)),
+            .map(|_| Neighbor::new(direction, Point::new(x + 1, y + 1))),
     }
 }
 
-pub fn neighbors<T>(grid: &[Vec<T>], x: usize, y: usize, include_diagonals: bool) -> Vec<Neighbor> {
+pub fn neighbors<T>(grid: &[Vec<T>], position: Point, include_diagonals: bool) -> Vec<Neighbor> {
     let mut directions = vec![
         Direction::Up,
         Direction::Down,
@@ -165,21 +200,23 @@ pub fn neighbors<T>(grid: &[Vec<T>], x: usize, y: usize, include_diagonals: bool
 
     directions
         .into_iter()
-        .filter_map(|d| neighbor_in_direction(grid, d, x, y))
+        .filter_map(|d| neighbor_in_direction(grid, d, position))
         .collect()
 }
 
-pub fn print_grid<T, F, O>(grid: &[Vec<T>], mapper: F)
+pub fn print_grid<T, F, O, W>(grid: &[Vec<T>], mapper: F, writer: &mut W) -> std::io::Result<()>
 where
     F: Fn(&T) -> O,
     O: Display,
+    W: Write,
 {
     for row in grid {
         for col in row {
-            print!("{}", mapper(col));
+            write!(writer, "{}", mapper(col))?;
         }
-        println!();
+        writeln!(writer)?;
     }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -198,134 +235,145 @@ mod tests {
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Right, 1, 0),
-                Neighbor::new(Direction::Down, 0, 1),
+                Neighbor::new(Direction::Right, Point::new(1, 0)),
+                Neighbor::new(Direction::Down, Point::new(0, 1)),
             ],
-            neighbors(&grid, 0, 0, false),
+            neighbors(&grid, Point::new(0, 0), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Right, 1, 0),
-                Neighbor::new(Direction::Down, 0, 1),
-                Neighbor::new(Direction::LowerRight, 1, 1),
+                Neighbor::new(Direction::Right, Point::new(1, 0)),
+                Neighbor::new(Direction::Down, Point::new(0, 1)),
+                Neighbor::new(Direction::LowerRight, Point::new(1, 1)),
             ],
-            neighbors(&grid, 0, 0, true),
+            neighbors(&grid, Point::new(0, 0), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Left, 4, 0),
-                Neighbor::new(Direction::Right, 6, 0),
-                Neighbor::new(Direction::Down, 5, 1),
+                Neighbor::new(Direction::Left, Point::new(4, 0)),
+                Neighbor::new(Direction::Right, Point::new(6, 0)),
+                Neighbor::new(Direction::Down, Point::new(5, 1)),
             ],
-            neighbors(&grid, 5, 0, false),
+            neighbors(&grid, Point::new(5, 0), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Left, 4, 0),
-                Neighbor::new(Direction::Right, 6, 0),
-                Neighbor::new(Direction::Down, 5, 1),
-                Neighbor::new(Direction::LowerLeft, 4, 1),
-                Neighbor::new(Direction::LowerRight, 6, 1),
+                Neighbor::new(Direction::Left, Point::new(4, 0)),
+                Neighbor::new(Direction::Right, Point::new(6, 0)),
+                Neighbor::new(Direction::Down, Point::new(5, 1)),
+                Neighbor::new(Direction::LowerLeft, Point::new(4, 1)),
+                Neighbor::new(Direction::LowerRight, Point::new(6, 1)),
             ],
-            neighbors(&grid, 5, 0, true),
+            neighbors(&grid, Point::new(5, 0), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Left, 8, 0),
-                Neighbor::new(Direction::Down, 9, 1),
+                Neighbor::new(Direction::Left, Point::new(8, 0)),
+                Neighbor::new(Direction::Down, Point::new(9, 1)),
             ],
-            neighbors(&grid, 9, 0, false),
+            neighbors(&grid, Point::new(9, 0), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Left, 8, 0),
-                Neighbor::new(Direction::Down, 9, 1),
-                Neighbor::new(Direction::LowerLeft, 8, 1),
+                Neighbor::new(Direction::Left, Point::new(8, 0)),
+                Neighbor::new(Direction::Down, Point::new(9, 1)),
+                Neighbor::new(Direction::LowerLeft, Point::new(8, 1)),
             ],
-            neighbors(&grid, 9, 0, true),
+            neighbors(&grid, Point::new(9, 0), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Up, 0, 4),
-                Neighbor::new(Direction::Down, 0, 6),
-                Neighbor::new(Direction::Right, 1, 5),
+                Neighbor::new(Direction::Up, Point::new(0, 4)),
+                Neighbor::new(Direction::Down, Point::new(0, 6)),
+                Neighbor::new(Direction::Right, Point::new(1, 5)),
             ],
-            neighbors(&grid, 0, 5, false),
+            neighbors(&grid, Point::new(0, 5), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Up, 0, 4),
-                Neighbor::new(Direction::Down, 0, 6),
-                Neighbor::new(Direction::Right, 1, 5),
-                Neighbor::new(Direction::UpperRight, 1, 4),
-                Neighbor::new(Direction::LowerRight, 1, 6),
+                Neighbor::new(Direction::Up, Point::new(0, 4)),
+                Neighbor::new(Direction::Down, Point::new(0, 6)),
+                Neighbor::new(Direction::Right, Point::new(1, 5)),
+                Neighbor::new(Direction::UpperRight, Point::new(1, 4)),
+                Neighbor::new(Direction::LowerRight, Point::new(1, 6)),
             ],
-            neighbors(&grid, 0, 5, true),
+            neighbors(&grid, Point::new(0, 5), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Up, 0, 8),
-                Neighbor::new(Direction::Right, 1, 9),
+                Neighbor::new(Direction::Up, Point::new(0, 8)),
+                Neighbor::new(Direction::Right, Point::new(1, 9)),
             ],
-            neighbors(&grid, 0, 9, false),
+            neighbors(&grid, Point::new(0, 9), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Up, 0, 8),
-                Neighbor::new(Direction::Right, 1, 9),
-                Neighbor::new(Direction::UpperRight, 1, 8),
+                Neighbor::new(Direction::Up, Point::new(0, 8)),
+                Neighbor::new(Direction::Right, Point::new(1, 9)),
+                Neighbor::new(Direction::UpperRight, Point::new(1, 8)),
             ],
-            neighbors(&grid, 0, 9, true),
+            neighbors(&grid, Point::new(0, 9), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Left, 3, 4),
-                Neighbor::new(Direction::Up, 4, 3),
-                Neighbor::new(Direction::Down, 4, 5),
-                Neighbor::new(Direction::Right, 5, 4),
+                Neighbor::new(Direction::Left, Point::new(3, 4)),
+                Neighbor::new(Direction::Up, Point::new(4, 3)),
+                Neighbor::new(Direction::Down, Point::new(4, 5)),
+                Neighbor::new(Direction::Right, Point::new(5, 4)),
             ],
-            neighbors(&grid, 4, 4, false),
+            neighbors(&grid, Point::new(4, 4), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::UpperLeft, 3, 3),
-                Neighbor::new(Direction::Left, 3, 4),
-                Neighbor::new(Direction::LowerLeft, 3, 5),
-                Neighbor::new(Direction::Up, 4, 3),
-                Neighbor::new(Direction::Down, 4, 5),
-                Neighbor::new(Direction::UpperRight, 5, 3),
-                Neighbor::new(Direction::Right, 5, 4),
-                Neighbor::new(Direction::LowerRight, 5, 5),
+                Neighbor::new(Direction::UpperLeft, Point::new(3, 3)),
+                Neighbor::new(Direction::Left, Point::new(3, 4)),
+                Neighbor::new(Direction::LowerLeft, Point::new(3, 5)),
+                Neighbor::new(Direction::Up, Point::new(4, 3)),
+                Neighbor::new(Direction::Down, Point::new(4, 5)),
+                Neighbor::new(Direction::UpperRight, Point::new(5, 3)),
+                Neighbor::new(Direction::Right, Point::new(5, 4)),
+                Neighbor::new(Direction::LowerRight, Point::new(5, 5)),
             ],
-            neighbors(&grid, 4, 4, true),
+            neighbors(&grid, Point::new(4, 4), true),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::Up, 9, 8),
-                Neighbor::new(Direction::Left, 8, 9),
+                Neighbor::new(Direction::Up, Point::new(9, 8)),
+                Neighbor::new(Direction::Left, Point::new(8, 9)),
             ],
-            neighbors(&grid, 9, 9, false),
+            neighbors(&grid, Point::new(9, 9), false),
         );
 
         assert_eq_ignore_order(
             vec![
-                Neighbor::new(Direction::UpperLeft, 8, 8),
-                Neighbor::new(Direction::Up, 9, 8),
-                Neighbor::new(Direction::Left, 8, 9),
+                Neighbor::new(Direction::UpperLeft, Point::new(8, 8)),
+                Neighbor::new(Direction::Up, Point::new(9, 8)),
+                Neighbor::new(Direction::Left, Point::new(8, 9)),
             ],
-            neighbors(&grid, 9, 9, true),
+            neighbors(&grid, Point::new(9, 9), true),
         );
+    }
+
+    #[test]
+    fn test_print_grid_to_writer() {
+        let grid = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+
+        let mut buffer = Vec::new();
+        print_grid(&grid, |x| *x, &mut buffer).unwrap();
+
+        let output = String::from_utf8(buffer).unwrap();
+        assert_eq!(output, "123\n456\n789\n");
     }
 }
